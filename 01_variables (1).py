a2a2838f61f7dadc1773614e5a98b28430f92df6
dracn79 <rubenjordaan@rhs.school.nz>
2025-02-14 14:42:01 +1300

    Update 01_variables (1).py
    updated the file quickly

---
+++ 01_variables (1).py
@@ -0,0 +1,10 @@
+#01_variables
+#Use sensible, descriptive variable names
+#Variable names in python are always lowercase
+#Constant names are UPPERCASE
+#Varibles can include a mix of letters,
+#underscores and numbers
+#(as long as the first character is not a number)
+
+v4ar14b12 = "Weird Variable"
+CONSTANT = "I love Computer Science"
