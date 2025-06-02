4f2b1e5f453cc05c73a654c36f76e33dfda07e2f
dracn79 <rubenjordaan@rhs.school.nz>
2025-06-03 10:26:38 +1200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,239 @@
+import random
+import time
+import board
+import digitalio
+
+led1 = digitalio.DigitalInOut(board.GP15)
+led1.direction = digitalio.Direction.OUTPUT
+
+led2 = digitalio.DigitalInOut(board.GP14)
+led2.direction = digitalio.Direction.OUTPUT
+
+led3 = digitalio.DigitalInOut(board.GP13)
+led3.direction = digitalio.Direction.OUTPUT
+
+led4 = digitalio.DigitalInOut(board.GP12)
+led4.direction = digitalio.Direction.OUTPUT
+
+led5 = digitalio.DigitalInOut(board.GP11)
+led5.direction = digitalio.Direction.OUTPUT
+
+button1 = digitalio.DigitalInOut(board.GP1)
+button1.direction = digitalio.Direction.INPUT
+button1.pull = digitalio.Pull.UP
+
+button2 = digitalio.DigitalInOut(board.GP0)
+button2.direction = digitalio.Direction.INPUT
+button2.pull = digitalio.Pull.UP
+
+powersystem_check = 1
+loop = 0
+
+while True:
+    if button1.value is True:
+
+        if button2.value is False:
+            if powersystem_check == 1:
+                powersystem_check = powersystem_check - 1
+
+            elif powersystem_check == 0:
+                powersystem_check = powersystem_check + 1
+
+        elif powersystem_check == 0:
+
+            led1.value = True
+            led2.value = True
+            led3.value = True
+            led4.value = True
+            led5.value = True
+            time.sleep(0.05)
+            led1.value = False
+            led2.value = False
+            led3.value = False
+            led4.value = False
+            led5.value = False
+            time.sleep(0.05)
+
+        else:
+            time_diffaculty = 0.1
+            points = 0
+            lap = 1
+            led5.value = True
+            time.sleep(.1)
+            led2.value = False
+            time.sleep(.1)
+
+            led4.value = True
+            time.sleep(.1)
+            led1.value = False
+            time.sleep(.1)
+
+            led3.value = True
+            time.sleep(.1)
+            led5.value = False
+            time.sleep(.1)
+
+            led2.value = True
+            time.sleep(.1)
+            led4.value = False
+            time.sleep(.1)
+
+            led1.value = True
+            time.sleep(.1)
+            led3.value = False
+            time.sleep(.1)
+
+    else:
+        if points == 10 * lap:
+            loop = lap
+            while loop == 0:
+                lap = lap + 1
+                led5.value = True
+                time.sleep(.1)
+                led2.value = False
+                time.sleep(.1)
+
+                led4.value = True
+                time.sleep(.1)
+                led1.value = False
+                time.sleep(.1)
+
+                led3.value = True
+                time.sleep(.1)
+                led5.value = False
+                time.sleep(.1)
+
+                led2.value = True
+                time.sleep(.1)
+                led4.value = False
+                time.sleep(.1)
+
+                led1.value = True
+                time.sleep(.1)
+                led3.value = False
+                time.sleep(.1)
+                loop -= 1
+
+        led1.value = False
+        led2.value = False
+        led3.value = False
+        led4.value = False
+        led5.value = False
+        points = points + 1
+        time_diffaculty -= 0.001
+        time.sleep(random.randint(1, 10))
+
+        led1.value = True
+        time.sleep(time_diffaculty)
+        if button1.value is False:
+            led1.value = False
+            led2.value = False
+            led3.value = False
+            led4.value = False
+            led5.value = False
+        else:
+            led2.value = True
+            time.sleep(time_diffaculty)
+            if button1.value is False:
+                led1.value = False
+                led2.value = False
+                led3.value = False
+                led4.value = False
+                led5.value = False
+            else:
+                led3.value = True
+                time.sleep(time_diffaculty)
+                if button1.value is False:
+                    led1.value = False
+                    led2.value = False
+                    led3.value = False
+                    led4.value = False
+                    led5.value = False
+                else:
+                    led4.value = True
+                    time.sleep(time_diffaculty)
+                    if button1.value is False:
+                        led1.value = False
+                        led2.value = False
+                        led3.value = False
+                        led4.value = False
+                        led5.value = False
+                    else:
+                        led5.value = True
+                        time.sleep(time_diffaculty)
+                        if button1.value is False:
+                            led1.value = False
+                            led2.value = False
+                            led3.value = False
+                            led4.value = False
+                            led5.value = False
+                        else:
+                            loop = loop = 0
+                            while loop != 60:
+                                led1.value = True
+                                time.sleep(0.01)
+                                led1.value = False
+                                time.sleep(0.01)
+                                led2.value = True
+                                time.sleep(0.01)
+                                led2.value = False
+                                time.sleep(0.01)
+                                led3.value = True
+                                time.sleep(0.01)
+                                led3.value = False
+                                time.sleep(0.01)
+                                led4.value = True
+                                time.sleep(0.01)
+                                led4.value = False
+                                time.sleep(0.01)
+                                led5.value = True
+                                time.sleep(0.01)
+                                led5.value = False
+                                time.sleep(0.01)
+                                loop += 1
+
+                            loop = points - 4
+                            while loop == 0:
+                                led1.value = False
+                                led2.value = False
+                                led3.value = False
+                                led4.value = False
+                                led5.value = False
+                                time.sleep(1)
+                                if points == points + 1:
+                                    led1.value = True
+                                    time.sleep(0.01)
+                                    led1.value = False
+                                    time.sleep(0.01)
+                                elif points == points + 2:
+                                    led1.value = True
+                                if points == points + 3:
+                                    led2.value = True
+                                    time.sleep(0.01)
+                                    led2.value = False
+                                    time.sleep(0.01)
+                                elif points == points + 4:
+                                    led2.value = True
+                                if points == points + 5:
+                                    led3.value = True
+                                    time.sleep(0.01)
+                                    led3.value = False
+                                    time.sleep(0.01)
+                                elif points == points + 6:
+                                    led3.value = True
+                                if points == points + 7:
+                                    led4.value = True
+                                    time.sleep(0.01)
+                                    led4.value = False
+                                    time.sleep(0.01)
+                                elif points == points + 8:
+                                    led4.value = True
+                                if points == points + 9:
+                                    led5.value = True
+                                    time.sleep(0.01)
+                                    led5.value = False
+                                    time.sleep(0.01)
+                                elif points == points + 10:
+                                    led5.value = True
+                                time.sleep(3)
+                                loop -= 1
